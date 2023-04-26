930da18c2a221f71949c5b33cdddf118e27dc46b
Adrián Ureña Umaña <aurena@solucionesinra.com>
2023-04-26 10:24:18 -0600

    cambios tailwind - temporales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'tw-',
-  content: ["./src/**/*.{html,ts}",],
+  mode: 'jit',
+  prefix:'tw-',
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/app/ModulosSistema/RiesgoCredito/Mantenimientos/**/*.{html,ts}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
+  plugins: [],
 }
 
