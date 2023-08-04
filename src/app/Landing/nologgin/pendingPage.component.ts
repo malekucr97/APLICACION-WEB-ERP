bdc0276fc26cfbb45a6366c0a8430d423f23ff60
Adrián Ureña Umaña <aurena@solucionesinra.com>
2023-08-04 13:44:15 -0600

    reenvio de correo de activación

--- src/app/Landing/nologgin/pendingPage.component.ts
+++ src/app/Landing/nologgin/pendingPage.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { AccountService } from '@app/_services';
 import { User } from '@app/_models';
+import { first } from 'rxjs/operators';
 
-@Component({ templateUrl: 'HTML_PendingPage.html', 
-             styleUrls: ['../../../assets/scss/landing/app.scss'] 
+@Component({
+  templateUrl: 'HTML_PendingPage.html',
+  styleUrls: ['../../../assets/scss/landing/app.scss'],
 })
 export class PendingUserPageComponent {
-    user = new User();
-    constructor(private accountService: AccountService) { this.user = this.accountService.userValue; }
-    redirect() { this.accountService.logout(); }
+  user = new User();
+
+
+  private _mostrarMensajeReenvio : boolean;
+  public get mostrarMensajeReenvio() : boolean {
+    return this._mostrarMensajeReenvio;
+  }
+  public set mostrarMensajeReenvio(v : boolean) {
+    this._mostrarMensajeReenvio = v;
+  }
+
+  private _mensajeReenvio : string;
+  public get mensajeReenvio() : string {
+    return this._mensajeReenvio;
+  }
+  public set mensajeReenvio(v : string) {
+    this._mensajeReenvio = v;
+  }
+
+
+  constructor(private accountService: AccountService) {
+    this.user = this.accountService.userValue;
+  }
+
+  redirect() {
+    this.accountService.logout();
+  }
+
+  reenviarEmail() {
+    this.mostrarMensajeReenvio = false;
+    this.accountService.reenviarCorreoActivacion(
+      this.user.identificacion,
+      'n/a'
+      )
+      .pipe(first())
+      .subscribe((response) => {
+        this.mensajeReenvio = response.responseMesagge;
+        this.mostrarMensajeReenvio = true;
+      });
+  }
+
 }
