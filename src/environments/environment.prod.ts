05222bb85d81179a3506c44fd1aa2b08c1ad2645
Marco López <mlopez@solucionesinra.com>
2023-08-10 12:12:27 -0600

    COMMIT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'http://10.100.242.70:81',
+    apiUrl: 'http://10.100.242.70:10405',
 
     sessionStorageIdentificationUserKey: '_httpUsernameSessionStorageAppAdminModulesInraSA',
     sessionStorageModuleIdentification : '_httpModuleSelectedSessionStorageAppAdminModulesInraSA'
